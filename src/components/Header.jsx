610b7284c8be95f7927f9b16015c1ca48e18854d
Juan Cruz <jclopezponce@axter.com.ar>
2025-02-12 16:54:00 +0100

    fix style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,7 @@ import React from "react";
 export default function Header(){
     return (
         <header>
-            <h1>
             To Do List
-            </h1>
-            
         </header>
     )
 }
